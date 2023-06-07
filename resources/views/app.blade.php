8d3b856389efe799e7793f899d76883a5b66b7f7
taimoornaveed <mtaimoornaveed@gmail.com>
2023-06-07 21:01:58 +0500

    updated public path

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('public/assets/css/bootstrap.min.css')}}">
     <title>@yield('title')</title>
     <style>
         .leading-5 {
@@ -30,9 +30,9 @@
     </div>
     @yield('content')
     @yield('modal')
-    <script type="text/javascript" src="{{asset('assets/js/jquery.min.js')}}"></script>
-    <script type="text/javascript" src="{{asset('assets/js/ajax.jquery.min.js')}}"></script>
-    <script type="text/javascript" src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('public/assets/js/jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('public/assets/js/ajax.jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('public/assets/js/bootstrap.bundle.min.js')}}"></script>
     @yield('scripts')
 </body>
 </html>
